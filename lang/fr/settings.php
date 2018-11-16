ef1f6982e8cbaf15ede4f25973f1d3f351b151ce
Nicolas Friedli <nicolas@theologique.ch>
2018-11-16 20:30:23 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['whenToHide_revision']   = 'Anciennes révisions';
+$lang['whenToHide_recent']     = 'Modifications récentes';
+$lang['whenToHide_diff']       = 'Montrer les diffs';
+$lang['excludeMgAndSp_o_sp']   = 'Super-utilisateurs';
+$lang['excludeMgAndSp_o_mg']   = 'Gestionnaires (dont super-utilisateurs)';
